7120105cb267666938e774f9e87e3a32bf0e2e7a
HBSISGustavoHamerschmidt <gustavo.h@hbsis.com.br>
2020-09-01 14:55:09 -0300

    changes
    Changes

--- teams/padawan-2020-maringa/csharp/gigasecond/Gigasecond.cs
+++ teams/padawan-2020-maringa/csharp/gigasecond/Gigasecond.cs
@@ -2,8 +2,5 @@ using System;
 
 public static class Gigasecond
 {
-    public static DateTime Add(DateTime moment)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static DateTime Add(DateTime moment) => moment.Add(new TimeSpan(0, 0, 0, 1000000000, 0));
 }

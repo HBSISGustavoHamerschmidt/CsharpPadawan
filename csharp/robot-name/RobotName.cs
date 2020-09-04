dea7e9b10e2ba455abeca23129a3b970bd98e403
HBSISGustavoHamerschmidt <gustavo.h@hbsis.com.br>
2020-09-04 09:48:30 -0300

    Finished Robot

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,17 +1,35 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Robot
 {
-    public string Name
-    {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
+    private readonly Random _rnd = new Random();
+
+    private static readonly List<string> List = new List<string>();
+
+    public Robot() => Reset();
+
+    public string Name { get; set; }
 
     public void Reset()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        while (true)
+        {
+            string robotNameNew = string.Empty;
+            for (int i = 0; i < 2; i++)
+            {
+                char randomChar = (char)_rnd.Next('A', 'Z');
+                robotNameNew += randomChar;
+            }
+            robotNameNew += _rnd.Next(999);
+
+            if (List.Any(q => q == robotNameNew)) continue;
+
+            List.Add(robotNameNew);
+            Name = robotNameNew;
+            break;
+        }
+
     }
 }

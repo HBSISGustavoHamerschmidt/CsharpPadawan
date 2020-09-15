2987e7988906ea560c226ef48bed214d009c18c3
HBSISGustavoHamerschmidt <gustavo.h@hbsis.com.br>
2020-09-15 16:11:12 -0300

    Small changes to make the code cleaner

--- teams/padawan-2020-maringa/csharp/tree-building/TreeBuilding.cs
+++ teams/padawan-2020-maringa/csharp/tree-building/TreeBuilding.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
 public class TreeBuildingRecord
 {
     public int ParentId { get; set; }
     public int RecordId { get; set; }
 }
-
 public class Tree
 {
     public int Id { get; set; }
@@ -15,20 +13,19 @@ public class Tree
     public List<Tree> Children { get; set; }
     public bool IsLeaf => Children.Count == 0;
 }
-
 public static class TreeBuilder
 {
     public static Tree BuildTree(IEnumerable<TreeBuildingRecord> records)
     {
         records = records.OrderBy(q => q.RecordId);
-
         var recordsList = records.ToList();
-        var recordListWhere = recordsList.Where((r, index) => r.RecordId != index).Any();
-        var recordListAny = recordsList.Any(r =>
-            (r.RecordId == 0 && r.ParentId != 0) ||
-            (r.RecordId != 0 && r.ParentId >= r.RecordId));
 
-        if (recordListAny || !recordsList.Any() || recordListWhere)
+        var recordListWhere = recordsList.Where((r, index) => 
+             r.RecordId != index || 
+            (r.RecordId == 0 && r.ParentId != 0) || 
+            (r.RecordId != 0 && r.ParentId >= r.RecordId)).Any();
+
+        if (!recordsList.Any() || recordListWhere) 
             throw new ArgumentException();
 
         var trees = new List<Tree>();

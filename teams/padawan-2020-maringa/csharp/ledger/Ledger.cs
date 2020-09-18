fc94051f15f17e4f43bc35fe3acf23cd8660a69e
HBSISGustavoHamerschmidt <gustavo.h@hbsis.com.br>
2020-09-17 22:27:06 -0300

    Update Ledger.cs

--- teams/padawan-2020-maringa/csharp/ledger/Ledger.cs
+++ teams/padawan-2020-maringa/csharp/ledger/Ledger.cs
@@ -19,36 +19,35 @@ public class LedgerEntry
 }
 
 public static class Ledger
-{
+{ 
     public static LedgerEntry CreateEntry(string date, string desc, int change) =>
         new LedgerEntry(DateTime.Parse(date, CultureInfo.InvariantCulture), desc, change / 100.0m);
 
     private static (CultureInfo, string) CreateCulture(string currency, string locale)
     {
-        int curNeg = 0;
-        string datPat = null;
-        string format = string.Empty;
-
-        if ((currency != "USD" && currency != "EUR") || (locale != "nl-NL" && locale != "en-US"))
-            throw new ArgumentException("Invalid currency");
-
+        int curNeg;
+        string datPat;
+        string format;
         string currencySymbol = currency switch
         {
             "USD" => "$",
             "EUR" => "€",
+            _ => throw new ArgumentException("Invalid currency")
         };
-
         switch (locale)
         {
             case "en-US":
+                curNeg = 0;
                 datPat = "MM/dd/yyyy";
-                format = $"{"Date"?.PadRight(11, ' ')}| {"Description"?.PadRight(26, ' ')}| {"Change"?.PadRight(13, ' ')}";
+                format = $"{"Date", -11}| {"Description", -26}| {"Change", -13}";
                 break;
             case "nl-NL":
                 curNeg = 12;
                 datPat = "dd/MM/yyyy";
-                format = $"{"Datum"?.PadRight(11, ' ')}| {"Omschrijving"?.PadRight(26, ' ')}| {"Verandering"?.PadRight(13, ' ')}"; ;
+                format = $"{"Datum",-11}| {"Omschrijving",-26}| {"Verandering",-13}";
                 break;
+            default:
+                throw new ArgumentException("Invalid locale");
         }
 
         var culture = new CultureInfo(locale)
@@ -67,12 +66,11 @@ public static class Ledger
             $"{entry.Description.Substring(0, 22)}..." :
             entry.Description;
 
-        var change = entry.Change < 0.0m ? entry.Change.ToString("C", culture) : $"{entry.Change.ToString("C", culture)} ";
+        var change = entry.Change < 0.0m ?
+            entry.Change.ToString("C", culture) :
+            $"{entry.Change.ToString("C", culture)} ";
 
-
-        var format = new StringBuilder();
-        format.Append(date).Append(" | ").Append($"{description,-25}").Append(" | ").Append($"{change,13}");
-        return format.ToString();
+        return string.Format("{0} | {1, -25} | {2, 13}", date, description, change);
     }
 
     private static IEnumerable<LedgerEntry> Sort(LedgerEntry[] entries)
